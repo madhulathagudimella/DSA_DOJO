8d45a603b00ff8ab50717be6cffbf8af7e99e47b
madhulatha <168632865+madhulathagudimella@users.noreply.github.com>
2026-06-08 11:37:02 +0530

    Time: 4 ms (51.01%), Space: 14.6 MB (8.36%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self,nums):
+        d={}
+        for n in nums:
+            d[n]=d.get(n,0)+1
+        for k,v in d.items():
+            if v==1:
+                return k
